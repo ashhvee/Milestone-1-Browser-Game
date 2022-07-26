1c28267e5dbfe4df2506877769e49b47daa862e9
Ashley Vieira <ash.adr24212@gmail.com>
2022-07-26 18:13:07 -0400

    running right

--- move.js
+++ move.js
@@ -1,5 +1,26 @@
 let stickmanBottom = 0
 let maxHeight = 50
+var speed = 6
+    
+moveStickman = function(moveBy) {
+    var stickman = document.getElementById('stickman'),
+        left = stickman.offsetLeft;
+      
+    if ((moveBy > 0 && left > 800) || (moveBy < 0 && left < 800)) {
+        clearTimeout(timer);
+        timer = setInterval(function() {
+            moveStickman(moveBy);
+        }, speed);
+      }
+      
+      stickman.style.left = left + moveBy + 'px';
+    };
+  
+var timer = setInterval(
+  function() { 
+    moveStickman(1); 
+  }
+  , speed);
 
 function jump(){
     var stickman = document.getElementById("stickman");
