84fa9a6d5fd6d2bdf1ae0660662d313734dce3b2
Ashley Vieira <ash.adr24212@gmail.com>
2022-07-27 00:19:12 -0400

    more adjustments

--- move.js
+++ move.js
@@ -1,43 +1,53 @@
 let stickmanBottom = 0
 let maxHeight = 50
-var speed = 6
-    
-moveStickman = function(moveBy) {
-    var stickman = document.getElementById('stickman'),
-        left = stickman.offsetLeft;
-      
-    if ((moveBy > 0 && left > 800) || (moveBy < 0 && left < 800)) {
-        clearTimeout(timer);
-        timer = setInterval(function() {
-            moveStickman(moveBy);
-        }, speed);
-      }
-      
-      stickman.style.left = left + moveBy + 'px';
-    };
-  
-var timer = setInterval(
-  function() { 
-    moveStickman(1); 
-  }
-  , speed);
+var speed = 7
 
+// stickman automatically runs right
+moveStickman = function(moveRight) {
+    var stickman = document.getElementById("stickman")
+    // console.log("moving")
+        left = stickman.offsetLeft
+      
+    if ((moveRight > 0 && left > 0) || (moveRight < 0 && left < 0)) {
+        clearTimeout(timer)
+        timer = setInterval(function() {
+            moveStickman(moveRight)
+        }, speed)
+    }
+        stickman.style.left = left + moveRight + "px"
+
+    // collision detection
+    let player = stickman.getBoundingClientRect()
+    console.log(stickman)
+    function detectCollision(stickman, obstacles) {
+        return !(
+        stickman.bottom < obstacles.top ||
+        stickman.right < obstacles.left
+        ) 
+    }
+}
+    var timer = setInterval(
+        function() { 
+            moveStickman(1.3)
+        }, speed)
+    
+// stickman jumps
 function jump(){
-    var stickman = document.getElementById("stickman");
-    // document.addEventListener("click", jump);
-    console.log("entering a jump function")
+    var stickman = document.getElementById("stickman")
+    // console.log("entering a jump function")
     let upInterval = setInterval(() => {
         if(stickmanBottom >= maxHeight ){
             clearInterval(upInterval)
             fall()
         }
-        stickmanBottom += 25
+        stickmanBottom += 32
         stickman.style.bottom = `${stickmanBottom}px`
     },100)
 }
 
+// stickman returning to floor after jump
 function fall(){
-    var stickman = document.getElementById("stickman");
+    var stickman = document.getElementById("stickman")
     let downInterval = setInterval(() => {
         if(stickmanBottom <= 10 ){
             clearInterval(downInterval)
@@ -46,3 +56,18 @@ function fall(){
         stickman.style.bottom = `${stickmanBottom}px`
     },100)
 }
+
+
+// const stickman = document.getElementById("stickman")
+// const obstacles = document.getElementById("obstacles").getBoundingClientRect()
+
+// console.log(stickman)
+
+// function detectCollision(stickman, obstacles) {
+//     return !(
+//             stickman.bottom < obstacles.top ||
+//             stickman.right < obstacles.left
+//         ) 
+// }
+
+// console.log(detectCollision(stickman))
